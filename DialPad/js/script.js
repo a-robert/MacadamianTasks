9d1d3276f9b08be2bc0072d029d3930daf8b6fe3
a-robert <asaturyan.robert@gmail.com>
2014-05-19 00:13:15 +0400

    DialPad

---
+++ DialPad/js/script.js
@@ -0,0 +1,66 @@
+function Person (name, number) {
+	var self = this;
+	
+	self.name = name;
+	self.number = number;
+}
+
+function ContactsModelView () {
+	var self = this;
+
+	var personList = [
+		new Person("Robert Asaturyan", "077504950")
+	];
+
+	self.contacts = ko.observableArray(personList);
+
+	self.addContactShow = ko.observable(true);
+
+	self.selectedPerson = ko.observable(null);
+
+	self.goToDialer = ko.observable(false);
+
+	self.addContactFormShow = ko.observable(false);
+
+	self.goToDialerTab = function () {
+		self.contacts(null);
+		self.goToDialer(true);
+		self.addContactShow(false);
+		self.addContactFormShow(false);
+	}
+
+
+	self.goToFullInfo = function (selectedPersonEvant) {
+		self.selectedPerson(selectedPersonEvant);
+		self.contacts(null);
+		self.addContactShow(false);
+		self.addContactFormShow(false);
+	}
+			
+	self.goToContactsTab = function () {
+		self.contacts(personList);
+		self.goToDialer(false);
+		self.selectedPerson(null);
+		self.addContactShow(true);
+		self.addContactFormShow(false);
+	}
+
+	self.addContact = function () {
+		self.addContactShow(false);
+		self.contacts(null);
+		self.addContactFormShow(true);
+	}
+
+	self.addNewContactToList = function () {
+		var newName = document.getElementById("newContactName").value;
+		var newNumber = document.getElementById("newContactNumber").value;
+		personList.push(new Person(newName, newNumber));
+		self.contacts(personList);
+		self.goToDialer(false);
+		self.selectedPerson(null);
+		self.addContactShow(true);
+		self.addContactFormShow(false);
+	}
+}
+
+ko.applyBindings(new ContactsModelView ());
